a4074bb375bfc523f8e54d8aae0fb457f12bec83
saiprudvish <rsaiprudvish123@gmail.com>
2022-01-18 20:32:50 +0530

    first react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react';
 
 function App() {
+  //state
+  
+//   const hello =()=>{
+//      console.log("thala")
+//   }
+//   const hello1 =(a)=>{
+//     console.log("thala",a)
+//  }
+// let [name,setName]=useState('navaneeth')
+// const changeName=()=>{
+//   setName('prudvish')
+// }
+let [user,setUser]=useState({
+  name:'prudvish',
+  city:'karimnagar'
+})
+const changeName=()=>{
+  setUser({...user,name:'rohan',city :'nizambad'})
+}
+
+
+   let users =[
+     {
+     id:1,
+     name:'prudvish',
+     city:'karimnagar'
+     },
+     {
+      id:2,
+      name:'rohan',
+      city:'nizambad'
+      },
+      {
+        id:3,
+        name:'likhith',
+        city:'luxxerpet'
+        },
+        {
+          id:4,
+          name:'pravardhan',
+          city:'hyderabad'
+          }
+   ]
+ // return react element
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //  <div className='text-center'>
+  //    <h1 className='text-danger'>hello guys</h1>
+  //    <h3 className='text-info'> i am {name}</h3>
+  //    <button className='btn btn-success' onClick={hello}> hello </button>
+  //    <button className='btn btn-success' onClick={()=> hello1(100)}> hello </button>
+  //  </div>
+  // <div className='text-center text-info'>
+  //   <h1>{name}</h1>
+  //   <button className='btn btn-success' onClick={changeName}>click me</button>
+  // </div>
+//   <div className='text-center text-info'>
+//     <h1>{user.name}</h1>
+//     <h1>{user.city}</h1>
+//     <button className='btn btn-success' onClick={changeName}>click me</button>
+//  </div>
+
+  <>
+    <div className='text-center text-info'>
+        <h1>{user.name}</h1>
+        <h1>{user.city}</h1>
+        <button className='btn btn-success' onClick={changeName}>click me</button>
+     </div>
+  
+  
+<div className='container'>
+     <table className='table text-center'>
+    <thead>
+      <tr>
+      <th>
+        id
+      </th>
+      <th>
+        name
+      </th>
+      <th>
+        city
+      </th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        users.map((userObj)=><tr key={userObj.id}>
+          <td>{userObj.id}</td>
+          <td>{userObj.name}</td>
+          <td>{userObj.city}</td>
+        </tr>)
+      }
+    </tbody>
+     </table>
+</div>
+</>
+
+ )
+
 }
 
 export default App;
