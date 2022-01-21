0d2d359f1be0b2658d0fdde970739251572da3e1
saiprudvish <rsaiprudvish123@gmail.com>
2022-01-21 13:17:04 +0530

    effect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import Child1 from './components/child1';
-import {useState} from 'react';
+import Useeffectdemo from "./components/Useeffectdemo"
+
+
+
 
 function App() {
-  //state
-  let [user,Setuser]=useState({
-    id:3248,
-    name:'prudvish',
-    city:'hyderabad'
-  })
-  let [child1Data,setchild1Data]=useState('')
-  const getData=(data)=>{
-    console.log('data got is',data)
-    setchild1Data(data)
-  }
-  // const getData(data)=>{
-  //   console.log(data)
-  // }
- // return react element
+   return(
+     <div className=' text-center'>
+      
 
-  return (
-<>
-  <h3>data i got from  {user.name} is {child1Data}</h3>
-   <Child1 user={user} getData={getData}></Child1>
-   </>
+       <Useeffectdemo/>
+       
+     
+      
 
+     </div>
+   )
+  
  
- )
-
 }
-
 export default App;
