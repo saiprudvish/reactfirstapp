066cbd8bb6bb585d3e3f20026f3f7a8e541fe5d2
saiprudvish <rsaiprudvish123@gmail.com>
2022-01-21 09:39:47 +0530

    usestae

--- src/App.js
+++ src/App.js
@@ -1,104 +1,30 @@
-
+import Child1 from './components/child1';
 import {useState} from 'react';
 
 function App() {
   //state
-  
-//   const hello =()=>{
-//      console.log("thala")
-//   }
-//   const hello1 =(a)=>{
-//     console.log("thala",a)
-//  }
-// let [name,setName]=useState('navaneeth')
-// const changeName=()=>{
-//   setName('prudvish')
-// }
-let [user,setUser]=useState({
-  name:'prudvish',
-  city:'karimnagar'
-})
-const changeName=()=>{
-  setUser({...user,name:'rohan',city :'nizambad'})
-}
-
-
-   let users =[
-     {
-     id:1,
-     name:'prudvish',
-     city:'karimnagar'
-     },
-     {
-      id:2,
-      name:'rohan',
-      city:'nizambad'
-      },
-      {
-        id:3,
-        name:'likhith',
-        city:'luxxerpet'
-        },
-        {
-          id:4,
-          name:'pravardhan',
-          city:'hyderabad'
-          }
-   ]
+  let [user,Setuser]=useState({
+    id:3248,
+    name:'prudvish',
+    city:'hyderabad'
+  })
+  let [child1Data,setchild1Data]=useState('')
+  const getData=(data)=>{
+    console.log('data got is',data)
+    setchild1Data(data)
+  }
+  // const getData(data)=>{
+  //   console.log(data)
+  // }
  // return react element
+
   return (
-  //  <div className='text-center'>
-  //    <h1 className='text-danger'>hello guys</h1>
-  //    <h3 className='text-info'> i am {name}</h3>
-  //    <button className='btn btn-success' onClick={hello}> hello </button>
-  //    <button className='btn btn-success' onClick={()=> hello1(100)}> hello </button>
-  //  </div>
-  // <div className='text-center text-info'>
-  //   <h1>{name}</h1>
-  //   <button className='btn btn-success' onClick={changeName}>click me</button>
-  // </div>
-//   <div className='text-center text-info'>
-//     <h1>{user.name}</h1>
-//     <h1>{user.city}</h1>
-//     <button className='btn btn-success' onClick={changeName}>click me</button>
-//  </div>
+<>
+  <h3>data i got from  {user.name} is {child1Data}</h3>
+   <Child1 user={user} getData={getData}></Child1>
+   </>
 
-  <>
-    <div className='text-center text-info'>
-        <h1>{user.name}</h1>
-        <h1>{user.city}</h1>
-        <button className='btn btn-success' onClick={changeName}>click me</button>
-     </div>
-  
-  
-<div className='container'>
-     <table className='table text-center'>
-    <thead>
-      <tr>
-      <th>
-        id
-      </th>
-      <th>
-        name
-      </th>
-      <th>
-        city
-      </th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        users.map((userObj)=><tr key={userObj.id}>
-          <td>{userObj.id}</td>
-          <td>{userObj.name}</td>
-          <td>{userObj.city}</td>
-        </tr>)
-      }
-    </tbody>
-     </table>
-</div>
-</>
-
+ 
  )
 
 }
